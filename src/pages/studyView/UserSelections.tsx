be3450880e9dffa5cb050f6bba14117087e3b8aa
kunalhemnani1 <kunalhemnani.kh@gmail.com>
2026-02-19 10:04:14 +0000

    Fix Survival Attribute unable to render Group Logic (#11935)
    * Fixes Group Logic for Survival Attribute like ER Status and HER2 Status not appearing when applied filter
    
    * Resolves survival attributeId by checking attributesMetaSet existence before assignment

--- src/pages/studyView/UserSelections.tsx
+++ src/pages/studyView/UserSelections.tsx
@@ -703,6 +703,13 @@ export default class UserSelections extends React.Component<
         return components;
     }
 
+    private resolveSurvivalAttributeId(attributeId: string) {
+        const survivalAttributeId = getSurvivalChartMetaId(attributeId);
+        return this.props.attributesMetaSet[survivalAttributeId]
+            ? survivalAttributeId
+            : attributeId;
+    }
+
     private renderClinicalDataFilters(
         filters: ClinicalDataFilter[],
         components: JSX.Element[],
@@ -714,13 +721,11 @@ export default class UserSelections extends React.Component<
                 const attributeId = isSurvivalAttributeId(
                     clinicalDataFilter.attributeId
                 )
-                    ? getSurvivalChartMetaId(clinicalDataFilter.attributeId)
+                    ? this.resolveSurvivalAttributeId(
+                          clinicalDataFilter.attributeId
+                      )
                     : clinicalDataFilter.attributeId;
-                const chartMeta =
-                    this.props.attributesMetaSet[attributeId] ||
-                    this.props.attributesMetaSet[
-                        clinicalDataFilter.attributeId
-                    ];
+                const chartMeta = this.props.attributesMetaSet[attributeId];
                 if (chartMeta) {
                     const dataType = this.props.clinicalAttributeIdToDataType[
                         clinicalDataFilter.attributeId
